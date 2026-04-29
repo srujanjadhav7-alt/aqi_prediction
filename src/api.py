fd3a7d708599ec7780af324f2c453a66e693d3ea
srujanjadhav7-alt <srujan.jadhav7@gmail.com>
2026-04-29 19:49:46 +0530

    fix cors

--- src/api.py
+++ src/api.py
@@ -3,6 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import os
 from src.predict import predict_aqi
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
